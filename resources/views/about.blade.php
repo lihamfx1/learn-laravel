0f04e5e8606ec1ea940c477a486a3aeaeec96f7c
lihamfx1 <hoacisjos48@gmail.com>
2020-11-22 23:29:38 +0700

    creating (about page)

---
+++ resources/views/about.blade.php
@@ -0,0 +1,23 @@
+@extends('master.master')
+
+@section('name', 'Home')
+
+@section('content')
+<div class="mt-4">
+  <div class="card">
+  <div class="card-body">
+    This is my web, I do whatever I need.
+  </div>
+</div>
+</div>
+<div class="mt-4">
+<div class="card text-center">
+  <div class="card-header bg-warning">
+    Thanks to
+  </div>
+  <div class="card-body">
+    <p class="card-text">Allah Swt.</p>
+  </div>
+</div>
+</div>
+@endsection
