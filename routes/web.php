01d3416f1f961124469b1cf047b94d300c67ca2b
lihamfx1 <hoacisjos48@gmail.com>
2020-11-22 23:17:51 +0700

    adding home and about,route

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,11 @@ use App\Http\Controllers\SiswaController;
 */
 // default
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
 });
 
 // closure
